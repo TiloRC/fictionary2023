7f8d6bb4121a4caf5603cf196a2f747328e018c5
Max Linden <maxlinden@optimum.net>
2022-12-22 15:41:46 -0500

    development push

--- react-app/my-app/src/App.js
+++ react-app/my-app/src/App.js
@@ -15,7 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React!!!!!!!!
+
         </a>
       </header>
     </div>
