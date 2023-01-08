b94958bd72e8ace44ffbd36100b86107b1e1e417
TiloRC <tiloreneau@gmail.com>
2023-01-08 13:46:27 -0800

    new look

--- react-app/my-app/src/App.js
+++ react-app/my-app/src/App.js
@@ -1,21 +1,38 @@
-
 import "./App.css";
 import DefList from "./components/defList";
-import './index.css';
-
+import "./index.css";
 
 function App() {
   return (
-    
     <div className="App">
-      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css"></link>
-      
-      <header className="App-header">Fictionary</header>
+      <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css"
+      ></link>
+      <div className="columns is-centered">
+        <div className="column is-three-quarters">
+          <header className="title">FICTIONARY</header>
+        </div>
+      </div>
 
-      <div>
-        <div className="App-dailyWord">[Daily Word]</div>
-
-        <DefList />
+      <div className="columns is-centered">
+        <div className="column is-three-quarters">
+          <div className="card">
+            <header className="card-header">
+              <p className="card-header-title">
+                Dailyword
+                <span className="px-1 has-text-weight-normal">
+                  is defined as:
+                </span>
+              </p>
+            </header>
+            <div className="card-content">
+              <div className="content">
+                <DefList />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- react-app/my-app/src/components/defList.jsx
+++ react-app/my-app/src/components/defList.jsx
@@ -5,24 +5,26 @@ class DefList extends Component {
     def: [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
       "Ut cursus hendrerit neque, vel blandit dui fermentum non.",
-      "Proin lectus lorem, tincidunt id urna vitae, elementum aliquam enim. Proin lectus lorem, tincidunt id urna vitae, elementum aliquam enim.",
+      "Proin lectus lorem, tincidunt id urna vitae, elementum aliquam enim. Proin lectus lorem, tincidunt id urna vitae, elementum aliquam enim. Proin lectus lorem, tincidunt id urna vitae, elementum aliquam enim.",
       "Maecenas velit ipsum, fringilla vel velit quis, congue varius augue.",
       " hmmVestibulum sapien ante, tincidunt eu sapien eget, fermentum finibus leo. 1324322435432132435",
     ],
   };
   render() {
     return (
-      <div class="columns is-multiline ">
+      <div className="columns is-multiline is-flex-direction-column">
         {this.state.def.map((def) => (
-          <button
-            class="App-definitions column  is-block is-light is-large is-responsive is-three-fifths is-offset-one-fifth is-flex-wrap-wrap"
-            onClick={() => {
-              alert("inline alert");
-            }}
-            key={def}
-          >
-            {def}
-          </button>
+          <div className="pt-1">
+            <button
+              class="column is-block is-light is-large is-responsive has-text-left is-full"
+              onClick={() => {
+                alert("inline alert");
+              }}
+              key={def}
+            >
+              {def}
+            </button>
+          </div>
         ))}
       </div>
     );
