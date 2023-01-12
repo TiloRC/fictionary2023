6290b3695425addfaf3992625ea8339359b88411
TiloRC <tiloreneau@gmail.com>
2023-01-11 16:59:13 -0800

    wip

--- react-app/my-app/src/components/defList.jsx
+++ react-app/my-app/src/components/defList.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class DefList extends Component {
   state = {
@@ -9,7 +10,18 @@ class DefList extends Component {
       "Returning, or flowing back; reflex; as, reflux action.",
       " One overattentive to forms, or too much confined to them; esp.,one who rests in external religious forms, or observes strictly theoutward forms of worship, without possessing the life and spirit ofreligion.As far a formalist from wisdom sits, In judging eyes, as libertinesfrom wits. Young.",
     ],
+    answer: [],
+    decoys: [],
   };
+
+  componentDidMount() {
+    axios.get(`http://127.0.0.1:8000/game/`).then((res) => {
+      const persons = res.data;
+      console.log(persons);
+      //this.setState({ persons });
+    });
+  }
+
   render() {
     return (
       <div className="columns is-multiline is-flex-direction-column">
@@ -21,7 +33,7 @@ class DefList extends Component {
               alert("inline alert");
             }}
           >
-            <div class="box py-2 px-2 is-clickable">
+            <div className="box py-2 px-2 is-clickable">
               <p>{def}</p>
             </div>
           </div>
